592a6cca3ff292c2420b71d63061fa6c60f1a217
Jerin Vishal B <73513972+jerinvishal@users.noreply.github.com>
2024-12-31 18:24:46 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -29,8 +29,8 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        origin: 'http://localhost:3000',
-        credentials: true,
+        origin: '*',
+        credentials: false,
     })
 );
 
